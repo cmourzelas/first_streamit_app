c9047e965d066d1c795a1fec2a4c0da80309aac1
cmourzelas <cmourzelas@yahoo.fr>
2023-07-31 13:30:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import snowflake.connector
 
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
